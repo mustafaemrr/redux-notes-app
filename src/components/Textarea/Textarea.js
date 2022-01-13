dbc495363173e56483d4e0f0e411857a6bf33fb9
Mustafa Emir <info@mustafaemir.com>
2022-01-13 22:08:07 +0300

    create textarea component

---
+++ src/components/Textarea/Textarea.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Textarea.scss';
+
+function Textarea() {
+  return (
+    <div className='textarea'>
+      <textarea name="textarea" />
+    </div>
+  )
+}
+
+export default Textarea
