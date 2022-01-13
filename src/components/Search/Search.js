94f07b7339f9ef7629cdcdeff2b22cfb04c9753d
Mustafa Emir <info@mustafaemir.com>
2022-01-13 22:08:38 +0300

    create search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Search.scss';
+
+function Search() {
+  return (
+    <div className='search'>
+      <input type='text' placeholder='Search ...' className='search__input'/>
+    </div>
+  )
+}
+
+export default Search
