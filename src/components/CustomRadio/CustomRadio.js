8220ba484c91f996674574b80f3f6f075adfeb88
Mustafa Emir <info@mustafaemir.com>
2022-01-13 22:09:25 +0300

    create customRadio component

---
+++ src/components/CustomRadio/CustomRadio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CustomRadio() {
+  return (
+    <>
+      <input class="custom-radio" type='radio' name="color"/>
+    </>
+  )
+}
+
+export default CustomRadio
