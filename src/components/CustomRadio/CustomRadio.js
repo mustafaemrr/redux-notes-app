3ee53596476bc8cbff1e3f59cebd57861b006f3d
Mustafa Emir <info@mustafaemir.com>
2022-01-15 23:58:32 +0300

    edit customRadio component

--- src/components/CustomRadio/CustomRadio.js
+++ src/components/CustomRadio/CustomRadio.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './CustomRadio.scss';
 
 function CustomRadio() {
   return (
     <>
-      <input class="custom-radio" type='radio' name="color"/>
+    <input id="radio1" type="radio" name="radio" className="hidden" />
+    <label htmlFor='radio1' className="flex items-center cursor-pointer text-xl">
+     <span className="w-8 h-8 inline-block mr-2 rounded-full border border-grey flex-no-shrink"></span>
+      Text
+     </label>
     </>
   )
 }
