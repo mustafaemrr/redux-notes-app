9d5a9bdc12e4df9d56633e42b2a6aa157366b8b3
Mustafa Emir <info@mustafaemir.com>
2022-01-15 23:59:32 +0300

    edit form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,32 @@
 import './Form.scss';
-import React from 'react'
 import Search from '../Search';
 import Textarea from '../Textarea';
+import CustomRadio from '../CustomRadio/';
 
 function Form() {
   return (
-      <div className='form'>
-        <blockquote class="text-2xl font-semibold italic text-center text-gray-900">
-          Redux 
-          <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-amber-500 relative inline-block mx-2">
-            <span class="relative text-white"> not </span>
-          </span>
-          uygulaması
+    <div className='form'>
+      <blockquote className="text-2xl font-semibold italic text-center text-gray-900">
+        Redux 
+        <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-amber-500 relative inline-block mx-2">
+          <span className="relative text-white"> not </span>
+        </span>
+        uygulaması
       </blockquote>
       <Search />
-      <Textarea />
+      
+      <div className="form__group">
+        <Textarea />
+
+        <div className="form__button-group">
+          <div className="form__radio-group">
+          <CustomRadio />
+          </div>
+          <div className="">
+            <button>Ekle</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
