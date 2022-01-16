080281316df560d4dd600634fd2f28e411092a87
Mustafa Emir <info@mustafaemir.com>
2022-01-17 00:06:55 +0300

    textarea component made dynamic

--- src/components/Textarea/Textarea.js
+++ src/components/Textarea/Textarea.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addDescription } from '../../redux/slice/notesSlice';
+
 import './Textarea.scss';
 
 function Textarea() {
+  const [description, setDescription] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addDescription(description))
+  }, [description]);
+
   return (
     <div className='textarea'>
-      <textarea name="textarea" />
+      <textarea name="textarea" placeholder='Notunuzu buraya girin...' onChange={(e) => setDescription(e.target.value)}/>
     </div>
   )
 }
