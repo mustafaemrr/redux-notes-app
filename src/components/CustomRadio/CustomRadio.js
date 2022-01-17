c77585429fecf5725d7209f9e7b7956ae6af4db9
Mustafa Emir <info@mustafaemir.com>
2022-01-17 22:44:10 +0300

    Fix CustomRadio component method

--- src/components/CustomRadio/CustomRadio.js
+++ src/components/CustomRadio/CustomRadio.js
@@ -1,25 +1,9 @@
-import React, { useState, useEffect } from 'react'
 import './CustomRadio.scss';
-import { useDispatch } from 'react-redux';
-import { addColor } from '../../redux/slice/notesSlice';
 
 function CustomRadio({color, id}) {
-  const [noteColor, setNoteColor] = useState('');
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(addColor(noteColor))
-  }, [dispatch, noteColor]);
-
-  const handleChange = e => {
-    const { value } = e.target;
-    
-    setNoteColor(value)
-  }
-
   return (
     <>
-      <input id={id} type="radio" name="color" value={color} className="hidden" onChange={handleChange} />
+      <input id={id} type="radio" name="color" value={color} className="hidden" />
       <label htmlFor={id} className="">
         <span className={color}></span>
       </label>
