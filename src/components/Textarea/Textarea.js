5c0bff19b8650bc03b642e271437ae591e788cd6
Mustafa Emir <info@mustafaemir.com>
2022-01-17 22:44:37 +0300

    fix textarea component method

--- src/components/Textarea/Textarea.js
+++ src/components/Textarea/Textarea.js
@@ -1,20 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { addDescription } from '../../redux/slice/notesSlice';
-
 import './Textarea.scss';
 
 function Textarea() {
-  const [description, setDescription] = useState('');
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(addDescription(description))
-  }, [dispatch, description]);
-
   return (
     <div className='textarea'>
-      <textarea name="textarea" placeholder='Notunuzu buraya girin...' onChange={(e) => setDescription(e.target.value)}/>
+      <textarea name="textarea" placeholder='Notunuzu buraya girin...' />
     </div>
   )
 }
