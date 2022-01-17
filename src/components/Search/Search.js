18bd52546b6817f7708938d9fb354cf051884de5
Mustafa Emir <info@mustafaemir.com>
2022-01-18 00:04:39 +0300

    add search component handle method

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Search.scss';
+import { useDispatch } from 'react-redux';
+import { filter } from '../../redux/slice/notesSlice';
+
 
 function Search() {
+  const [filterKey, setFilterKey] = useState('all');
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFilterKey(e.target.value);
+  }
+
+  useEffect(() => {
+    dispatch(filter(filterKey))
+  }, [filterKey])
+
+
   return (
     <div className='search'>
-      <input type='text' placeholder='Search ...' className='search__input'/>
+      <input type='text' placeholder='Search ...' className='search__input' onChange={handleChange}/>
     </div>
   )
 }
